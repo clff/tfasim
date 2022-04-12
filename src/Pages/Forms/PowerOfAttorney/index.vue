67c01e61b44def2d0618a44fd7c5cefd4c991b71
clff <clff313@gmail.com>
2022-04-12 10:32:23 +0300

    index files for first 2 forms

---
+++ src/Pages/Forms/PowerOfAttorney/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="power">
+    <h1>יפוי כח</h1>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.power {
+  background-color: green;
+  height: 500px;
+  width: 500px;
+
+  h1 {
+    text-align: center;
+  }
+}
+</style>
