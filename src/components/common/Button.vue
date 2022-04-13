f2769dc0237d9e36e4dfb1c6e012952f6f3bf9b5
clff <clff313@gmail.com>
2022-04-13 08:28:36 +0300

    created global btn component

---
+++ src/components/common/Button.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+});
+const emit = defineEmits(["action"]);
+
+const action = () => {
+  emit("action");
+};
+</script>
+
+<template>
+  <button @click="action">{{ text }}</button>
+</template>
+
+<style scoped lang='scss'>
+button {
+  cursor: pointer;
+  padding: var(--fw-2) var(--fw-5);
+  border: 1px solid var(--fw-gray-700);
+  border-radius: 10px;
+}
+</style>
