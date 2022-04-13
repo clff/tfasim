d4cf2e3753c69f3b0f306acba89dc16b0f143962
clff <clff313@gmail.com>
2022-04-13 07:31:59 +0300

    wip form client info + form lawyer info

--- src/Pages/Forms/Fees/TheFormClientInfo.vue
+++ src/Pages/Forms/Fees/TheFormClientInfo.vue
@@ -1,16 +1,24 @@
 <script setup>
+import feesService from "./feesService";
+
+const model = feesService.state.feesModel;
 </script>
 
 <template>
   <div class="client-container">
     <h1>client info</h1>
+    <FormInput labelText="שם פרטי" v-model="model.client.firstName" />
+    <FormInput labelText="שם משפחה" v-model="model.client.lastName" />
+  </div>
+  <div>
+    <Button />
   </div>
 </template>
 
 <style scoped lang='scss'>
 .client-container {
-  height: 500px;
-  width: 500px;
+  height: auto;
+  width: 100%;
   border: 1px solid var(--fw-black);
   border-radius: 10px;
 }
--- src/Pages/Forms/Fees/TheFormLawyerInfo.vue
+++ src/Pages/Forms/Fees/TheFormLawyerInfo.vue
@@ -1,11 +1,22 @@
 <script setup>
+import feesService from "./feesService";
+
+const model = feesService.state.feesModel;
 </script>
 
 <template>
-  <div>
-    <h1>lawyer info</h1>
+  <div class="client-container">
+    <h1>client info</h1>
+    <FormInput labelText="שם פרטי" v-model="model.lawyer.firstName" />
+    <FormInput labelText="שם משפחה" v-model="model.lawyer.lastName" />
   </div>
 </template>
 
 <style scoped lang='scss'>
+.client-container {
+  height: auto;
+  width: 100%;
+  border: 1px solid var(--fw-black);
+  border-radius: 10px;
+}
 </style>
