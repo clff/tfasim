375d2ca35f78f42a867cb5d20cf9a10d7dbce4ae
clff <clff313@gmail.com>
2022-04-13 07:32:16 +0300

    wip the results component

--- src/Pages/Forms/Fees/TheResults.vue
+++ src/Pages/Forms/Fees/TheResults.vue
@@ -1,8 +1,26 @@
 <script setup>
+import feesService from "./feesService";
+
+const model = feesService.state.feesModel;
 </script>
 
 <template>
-  <div class="results-container"></div>
+  <div class="results-container">
+    <h2>הסכם שכר טירחה</h2>
+    <div><span>שנערך ונחתם ביום:</span></div>
+    <div>
+      <span>בין:</span>
+      <span>{{ model.client.firstName }} {{ model.client.lastName }}</span>
+      <span>(להלן: ביחד ולחוד ״הלקוח״)</span>
+      <span>מצד אחד</span>
+    </div>
+    <div>
+      <span>לבין:</span>
+      <span>{{ model.lawyer.firstName }} {{ model.lawyer.lastName }}</span>
+      <span>(להלן: ״עוה״ד״)</span>
+      <span>מצד שני</span>
+    </div>
+  </div>
 </template>
 
 <style scoped lang='scss'>
